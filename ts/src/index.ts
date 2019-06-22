e59ee4deb9d52a508f8a656fee84c8701685a4a3
Bijou Trouvaille <sometimes@bijou4u.irc>
2019-06-21 19:10:16 -0500

    removed unnecessary assert import

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -1,20 +1,22 @@
 import $ = require('jquery');
-import { fail } from 'assert';
 
 function main() {
+
   const app = $('#app');
   const table = $('<table></table>');
+
   const els:JQuery<HTMLElement>[][] = [ ];
-  const cells: boolean[][] = []
+  const cells: boolean[][] = [ ];
+
   const edge = 5;
   let x = true;
   for (let i: number = 0; i < edge; i++) {
     let tr = $('<tr/>');
     for (let j: number = 0; j < edge; j++) {
       els[i] = els[i] || [];
-      cells[i] = cells[i] || []
+      cells[i] = cells[i] || [];
       const el = els[i][j] = $('<td>_</td>');
-      el.on('click', ()=>setCell(i,j))
+      el.on('click', ()=>setCell(i,j));
       tr.append(el)
       el.on('click', ()=>null)
     }
@@ -25,7 +27,7 @@ function main() {
   function setCell(i: number, j: number) {
     cells[i][j] = x;
     els[i][j].text(x ? 'X' : 'O');
-    els[i][j].off('click')
+    els[i][j].off('click');
     x = !x
   }    
 }
