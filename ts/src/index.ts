a1acc3d7ebc1e74ff8dd4b8a36c661b115158308
Bijou Trouvaille <sometimes@bijou4u.irc>
2019-08-03 16:57:28 -0500

    initializing the two-dimensional gridState array

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -10,6 +10,9 @@ import jquery = require('jquery');
    * 
    * ''2.
    * Create a function to get the state of each cell.
+   *   create a button that generates random a coordinate
+   *   show that coordinate on screen
+   *   show the state of a cell at that coordinate
    * Create a button to set a random cell to white.
    * Create a button to set a random cell to black.
    * 
@@ -17,26 +20,80 @@ import jquery = require('jquery');
 
 
 
-const N = 4;
+const N = 5;
+
+
 
 function main() {
 
+  // const numbers: number[][] = [] // two-dimensional array
+  // const n = numbers[12];
+  // console.log('n = ', n); // what is the value printed?
+  
+  const gridState: boolean[][] = [ ];
+  
+  function nextGen(cells: unknown) {
+    return cells
+  }
+  function getState(x: number, y: number): boolean {
+    // let state:boolean = ??? ;
+    // return state;
+    return false;
+  }
   const app = jquery('#app');
 
   const table = jquery('<table></table>');
   
+  const randomCellButton = jquery('<button>Check random cell</button>');
+  const resultBox = jquery('<span></span>');
+
+  app.append(randomCellButton);
+  app.append(resultBox);
+
+  randomCellButton.click(event=>{
+    event.preventDefault();
+    const x = Math.floor(Math.random() * N);
+    const y = Math.floor(Math.random() * N);
+    
+    const state = getState(x, y);
+    const stateDescr = state ? 'Black' : 'White'; // tertiary operator
+     
+    // let stateDescr: string;
+    // if (state == true) {
+    //   stateDescr = 'Black'
+    // } else {
+    //   stateDescr = 'White'
+    // }
+    
+    resultBox.text(
+      'coordinate = ' 
+      + x.toString() 
+      + ' : ' 
+      + y.toString() 
+      + '; '
+      + 'state = '
+      + stateDescr
+      );
+  })
   
   
   for (let row = 0; row < N; row++) {
     
+    
     const topRow = jquery('<tr></tr>');
     table.append(topRow);
+
+    gridState[row] = []; 
     
     for (let col = 0; col < N; col++) {
-      
+    
+      // col = 2
       const cell = jquery('<td></td>');
       topRow.append(cell);
       
+      // gridState[row] = []; // WRONG!
+      gridState[row][col] = true;
+    
       let state: boolean = true;
 
       cell.click(function(e) {
@@ -54,7 +111,7 @@ function main() {
     }
   }
 
-  
+  console.log('gridState', gridState);
 
   app.append(table);
 }
