0521ad8cea3d929fe6829573ef4718fbb9de1c6c
Bijou Trouvaille <sometimes@bijou4u.irc>
2019-09-02 09:52:38 -0500

    manipulate the state of the cell

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -8,6 +8,7 @@ import jquery = require('jquery');
    * Create an N x N grid of white cells.
    * Each one allows toggling to and from black via a click.
    * 
+   * Aug 17
    * ''2.
    * Create a function to get the state of each cell.
    *   create a button that generates a random coordinate
@@ -27,24 +28,27 @@ import jquery = require('jquery');
    *      - with help of unit testing
    * 
    * ''5. 
-   * Use the GoL function to bring the to life.
+   * Use the GoL function to bring the grid to life.
    */
 
 
 
-const N = 8;
-
+const N = 3;
+const ALIVE = false;
+const DEAD = true;
 
 
 function main() {
 
-  // const numbers: number[][] = [] // two-dimensional array
-  // const n = numbers[12];
-  // console.log('n = ', n); // what is the value printed?
-  
+  // this global variable will hold the information about our cell
   const gridState: boolean[][] = [ ];
+  // this global vairable will hold the HTML element of the cell
+  const gridCells: JQuery<HTMLElement>[][] = [ ];
 
-
+  // this function lets us see the information about the state.
+  // as we saw in our first "naive" attempt to toggle the state,
+  // this function can give wrong results if our visual and data 
+  // representations are de-synchronized.
   function getState(x: number, y: number): boolean {
     const state: boolean = gridState[y][x] ;
     return state;
@@ -54,34 +58,65 @@ function main() {
 
   const table = jquery('<table></table>');
   
-  const randomCellButton = jquery('<button>Check random cell</button>');
+  // this button should select a random cell and show its state
+  const showRandomCellButton = jquery('<button>Check random cell</button>');
+  // this button should select a random cell, flip its state and show the new one.
+  const setRandomCellButton = jquery('<button>Set random cell</button>');
+
   const resultBox = jquery('<span></span>');
 
-  app.append(randomCellButton);
+  app.append(showRandomCellButton);
+  app.append(setRandomCellButton);
   app.append(resultBox);
 
-  randomCellButton.click(event=>{
+  setRandomCellButton.click(event=>{
+    
+    event.preventDefault();
+
+    const col = Math.floor(Math.random() * N);
+    const row = Math.floor(Math.random() * N);
+
+    // get the current state
+    const state = getState(col, row);
+    
+    // make a constant to hold what the new state should be
+    const nextState = state === DEAD ? ALIVE : DEAD;
+
+    // describe in human-friendly text what the next state will be
+    const stateDescr = nextState === DEAD ? 'Dead' : 'Alive'; // tertiary operator
+
+    // set the data representation of the cell
+    gridState[row][col] = nextState;
+    // set the visual representation of the cell
+    gridCells[row][col].toggleClass('alive', nextState === ALIVE);
+
+    // summarize the results
+    resultBox.text(
+      'coordinate = ' 
+      + col.toString() 
+      + ' : ' 
+      + row.toString() 
+      + '; '
+      + 'state = '
+      + stateDescr
+      );
+  })
+
+  showRandomCellButton.click(event=>{
 
     event.preventDefault();
 
-    const x = Math.floor(Math.random() * N);
-    const y = Math.floor(Math.random() * N);
+    const col = Math.floor(Math.random() * N);
+    const row = Math.floor(Math.random() * N);
     
-    const isDead = getState(x, y);
-    const stateDescr = isDead ? 'White' : 'Black'; // tertiary operator
-     
-    // let stateDescr: string;
-    // if (state == true) {
-    //   stateDescr = 'Black'
-    // } else {
-    //   stateDescr = 'White'
-    // }
-    
+    const state = getState(col, row);
+    const stateDescr = state===DEAD ? 'Dead' : 'Alive'; // tertiary operator
+
     resultBox.text(
       'coordinate = ' 
-      + x.toString() 
+      + col.toString() 
       + ' : ' 
-      + y.toString() 
+      + row.toString() 
       + '; '
       + 'state = '
       + stateDescr
@@ -95,7 +130,10 @@ function main() {
     const topRow = jquery('<tr></tr>');
     table.append(topRow);
 
+    // initialize the data representation row
     gridState[row] = [];
+    // initialize the HTML element storage row
+    gridCells[row] = [];
 
 
     for (let col = 0; col < N; col++) {
@@ -103,27 +141,25 @@ function main() {
       const cell = jquery('<td></td>');
       topRow.append(cell);
       
-      // gridState[row] = []; // WRONG!
-      gridState[row][col] = true;
+      // initialize the state as dead
+      gridState[row][col] = DEAD;
+      // save the HTML element so that we can manipulate it later. 
+      gridCells[row][col] = cell;
       
       cell.click(function(e) {
         
         e.preventDefault();
 
-        if (gridState[row][col]) {
-          cell.toggleClass('black', true);
-          cell.toggleClass('white', false);
-          gridState[row][col] = false;
+        if (gridState[row][col] === DEAD) {
+          cell.toggleClass('alive', true);
+          gridState[row][col] = ALIVE;
         } else {
-          cell.toggleClass('black', false);
-          cell.toggleClass('white', true);
-          gridState[row][col] = true;
+          cell.toggleClass('alive', false);
+          gridState[row][col] = DEAD;
         }
       });
     }
   }
-
-  console.log('gridState', gridState);
 
   app.append(table);
 }
